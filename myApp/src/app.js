afd8e8c445ba4a98c0ed64a74a0ecdf85f3873c9
new name <new email>
2021-11-02 00:35:14 -0300

    test rename acc

--- myApp/src/app.js
+++ myApp/src/app.js
@@ -28,7 +28,7 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-//test
+
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
