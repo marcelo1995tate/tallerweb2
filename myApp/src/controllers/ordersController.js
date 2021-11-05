177eecae80d28bbe76d819ba3091c1b44934b7eb
Marcelo <Marcelo@DESKTOP-P76ICLL>
2021-11-05 01:25:04 -0300

    comiis

--- myApp/src/controllers/ordersController.js
+++ myApp/src/controllers/ordersController.js
@@ -3,27 +3,31 @@ const db = require('../../database/models');
 const { QueryTypes } = require('sequelize');
 
 let idUsuario = 1;
-let productosComprados=[1,2,3]
+let productosComprados = [1, 2, 3]
 const controller = {
 
     create: (req, res) => {
         db.Pedidos.create({ ID_USUARIO: idUsuario })
             .then((resultados) => {
-               db.sequelize.query('SELECT max(`ID_PEDIDO`) FROM `pedido` AS `Pedidos` LIMIT 1;', {
+                db.sequelize.query('SELECT max(`ID_PEDIDO`) FROM `pedido` AS `Pedidos` LIMIT 1;', {
                     type: QueryTypes.SELECT
-                  })
-                  return resultados.ID_PEDIDO
+                })
+                return resultados.ID_PEDIDO
             })
-            .then((idMax)=>{
+            .then((idMax) => {
                 productosComprados.forEach(element => {
-                    db.Items_Pedidos.create({ ID_PEDIDO: idMax,ID_PRODUCTO: element })
+                    db.Items_Pedidos.create({ ID_PEDIDO: idMax, ID_PRODUCTO: element })
                 })
                 res.send();
             })
     },
 
     getAll: (req, res) => {
-        db.Pedidos.findAll().then((resultados) => { res.json(resultados) })
+        db.sequelize.query('select * from pedido p inner join item_pedido ip on p.ID_PEDIDO=ip.ID_PEDIDO inner join producto pr on pr.ID_PRODUCTO=ip.ID_PRODUCTO WHERE ID_USUARIO='+idUsuario, {
+            type: QueryTypes.SELECT
+        }).then((resultados) => {
+            res.json(resultados)
+        })
     },
     getById: (req, res) => {
         db.Pedidos.findOne({
