76fc6304e471f5ac69e1119f089e4b71fed3a809
Marcelo <Marcelo@DESKTOP-P76ICLL>
2021-11-05 01:04:44 -0300

    agregar pedidos con auto_increment

--- myApp/src/routes/orders.js
+++ myApp/src/routes/orders.js
@@ -5,6 +5,8 @@ const ordersController = require ('../controllers/ordersController');
 
 /* GET users listing. */
 router.post('/create',ordersController.create);
+router.get('/getAll',ordersController.getAll);
+router.get('/getById/:id',ordersController.getById);
 
 
 module.exports = router;
