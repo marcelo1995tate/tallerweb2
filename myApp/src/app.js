ed6acf8bf4bfd4d580b0ee2082b0351617b09a92
Marcelo <Marcelo@DESKTOP-P76ICLL>
2021-11-06 00:51:54 -0300

    gitignore

--- myApp/src/app.js
+++ myApp/src/app.js
@@ -16,7 +16,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
 
-
 const productsRouter = require('./routes/products');
 const orderRouter = require('./routes/orders');
 
