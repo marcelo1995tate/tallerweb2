c111a6dec9bd1036c71e4c69e09dcb8b342eb608
Marcelo <Marcelo@DESKTOP-P76ICLL>
2021-11-06 21:28:11 -0300

    prueba de borrado total

--- myApp/src/routes/products.js
+++ myApp/src/routes/products.js
@@ -7,5 +7,6 @@ const productsController = require ('../controllers/productsController');
 /* GET users listing. */
 router.get('/mostar-todos',productsController.getAll);
 router.get('/mostar-producto/:id',productsController.getById);
+router.post('/create',productsController.create);
 
 module.exports = router;
