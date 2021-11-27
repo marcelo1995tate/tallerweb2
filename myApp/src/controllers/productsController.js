109b97de4f5da8a130dcc5abe9e3ec7e6b63440b
Marcelo <Marcelo@DESKTOP-P76ICLL>
2021-11-27 18:36:03 -0300

    validaciones agregar producto

--- myApp/src/controllers/productsController.js
+++ myApp/src/controllers/productsController.js
@@ -5,13 +5,13 @@ const controller = {
     getAll: (req, res) => {
         db.Productos.findAll().then((resultados) => { res.json(resultados) })
     },
-    
+
     getById: (req, res) => {
         db.Productos.findOne({
             where: {
                 ID_PRODUCTO: req.params.id
             }
-        }).then((resultados)=> {
+        }).then((resultados) => {
             if (!resultados) {
                 res.send('ID INEXISTENTE')
             }
@@ -19,9 +19,34 @@ const controller = {
         });
     },
     create: (req, res) => {
-        
-        db.Productos.create({ NOMBRE:req.body.NOMBRE,DESCRIPCION:req.body.DESCRIPCION,CLASIFICACION:req.body.CLASIFICACION,IMAGEN:req.file.filename,PRECIO:req.body.PRECIO})
-        res.send();
+        try {
+            let precio = parseInt(req.body.PRECIO, 10);
+
+            if (req.body.NOMBRE == '' || req.body.NOMBRE == null) {
+                throw 'campos invalidos'
+            }
+            if (req.body.DESCRIPCION == null || req.body.DESCRIPCION == '') {
+                throw 'campos invalidos'
+            }
+            if (req.body.CLASIFICACION == null || req.body.CLASIFICACION == '') {
+                throw 'campos invalidos'
+            }
+            if (precio == null || typeof (precio) != 'number' || isNaN(precio)) {
+                throw 'campos invalidos'
+            }
+            if (req.file.filename == null && req.file.filename == '') {
+                throw 'campos invalidos'
+            }
+
+            db.Productos.create({ NOMBRE: req.body.NOMBRE, DESCRIPCION: req.body.DESCRIPCION, CLASIFICACION: req.body.CLASIFICACION, IMAGEN: req.file.filename, PRECIO: precio })
+            res.send();
+
+        }
+        catch {
+            res.send('CAMPOS INVALIDOS')
+        }
+
+
     },
 }
 
--- myApp/src/routes/products.js
+++ myApp/src/routes/products.js
@@ -16,11 +16,11 @@ const storage = multer.diskStorage({
 })
 const upload = multer({
     storage, fileFilter: (req, file, cb) => {
-        if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
+        if ((file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg")) {
             cb(null, true);
         } else {
             cb(null, false);
-            return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
+            return cb(new Error('Imagenes en formato .png / .jpg / .jpeg'));
         }
     }
 });
