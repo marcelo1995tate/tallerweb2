0aa69606d7f284aa75f6b51068d6b2a98e3230af
Marcelo <Marcelo@DESKTOP-P76ICLL>
2021-11-30 00:04:35 -0300

    create order, visualizacion de imagenes

--- myApp/src/controllers/productsController.js
+++ myApp/src/controllers/productsController.js
@@ -19,8 +19,37 @@ const controller = {
         });
     },
     create: (req, res) => {
+        try {
+            let precio = parseInt(req.body.PRECIO, 10);
+
+            if (req.body.NOMBRE == '' || req.body.NOMBRE == null) {
+                console.log('----------------------nomb');
+                throw 'campos invalidos'
+            }
+            if (req.body.DESCRIPCION == null || req.body.DESCRIPCION == '') {
+                console.log('----------------------desc');
+                throw 'campos invalidos'
+            }
+            if (req.body.CLASIFICACION == null || req.body.CLASIFICACION == '') {
+                console.log('----------------------clase');
+                throw 'campos invalidos'
+            }
+            if (precio == null || typeof (precio) != 'number' || isNaN(precio)) {
+                console.log('----------------------precio');
+                throw 'campos invalidos'
+            }
+            if (req.file.filename == null && req.file.filename == '') {
+                console.log('----------------------img');
+                throw 'campos invalidos'
+            }
+            
             db.Productos.create({ NOMBRE: req.body.NOMBRE, DESCRIPCION: req.body.DESCRIPCION, CLASIFICACION: req.body.CLASIFICACION, IMAGEN: req.file.filename, PRECIO: precio })
             res.send();
+        }
+        catch {
+            res.send('CAMPOS INVALIDOS ')
+        }
+
     },
 }
 
